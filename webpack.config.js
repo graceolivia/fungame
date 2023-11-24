306a81f4304909792149ca0cb09a58fe202689cc
Grace <gracelidinskysmith@gmail.com>
2023-11-23 22:10:04 -0500

    Finish webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.html',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+    ]
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'docs'),
@@ -14,4 +23,9 @@ module.exports = {
     open: true,
     hot: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // Path to your source index.html
+    })
+  ],
 };
