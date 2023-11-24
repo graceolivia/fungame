5da8f034127d007fe10c462e7611b857a1ad46e9
Grace <gracelidinskysmith@gmail.com>
2023-11-23 21:57:54 -0500

    Rearrange file structure

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'docs'),
   },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'docs'), 
+      },
+    open: true,
+    hot: true,
+  },
 };
-s
